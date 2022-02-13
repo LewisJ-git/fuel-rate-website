f45b41abfac09f8c79dd874ba8dd56ffdea97a72
LongNguyen-git <lucasnguyen2112@gmail.com>
2022-02-12 21:06:54 -0600

    implement react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Fragment } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+    <header className="App-header">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+        </Routes>
       </header>
     </div>
   );
