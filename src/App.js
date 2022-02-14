b13dc8d6c52841310d1572dbad6177c72a15d16e
nhoang0925 <nhoang0925@gmail.com>
2022-02-13 20:39:17 -0600

    update project and title name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Fragment } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import NavigationBar from "./components/NavigationBar";
+import QuoteHistory from "./components/QuoteHistory";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
             <Fragment><Route path="/" element={<Home />} /></Fragment>
             <Fragment><Route path="/login" element={<Login />} /></Fragment>
+            <Fragment><Route path="/history" element={<QuoteHistory />} /></Fragment>
         </Routes>
       </header>
     </div>
---
+++ src/components/QuoteHistory.js
@@ -0,0 +1,14 @@
+import { Divider } from '@material-ui/core';
+import { Component } from 'react';
+
+class QuoteHistory extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My Header</h1>
+            </div>
+        )
+    }
+}
+
+export default QuoteHistory;
