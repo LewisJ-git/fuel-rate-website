7793272368d5a0aecfe98cf289eb6972090c327b
nhoang0925 <nhoang0925@gmail.com>
2022-02-14 01:28:22 -0600

    homepage added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import React from 'react';
+import fuel from "../images/fuel.jpg";
+import background from "../images/background.jpg"
 
 const Home = () => {
     return (
         <div>
-            <h3>Home</h3>
+            <h3 style={{ color: 'red'}}>Welcome to Coogs Fuel</h3>
+            <h4>Please login to get your competitive fuel quote!</h4>
+            <img src={fuel}
+            style = {{
+                width: '400px',
+                height: '400px',
+                objectFit:  'cover'
+            }} />
         </div>
+
     )
 }
 
