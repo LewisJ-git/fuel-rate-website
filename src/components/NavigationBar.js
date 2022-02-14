f3273fcc785137a67a4edc6aad2af967421876d2
LongNguyen-git <lucasnguyen2112@gmail.com>
2022-02-13 23:39:34 -0600

    Update NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,6 +7,7 @@ const NavigationBar = () => {
             <ul>
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/login'}>Login</Link></li>
+                <li><Link to={'/history'}>Quote History</Link></li>
             </ul>
         </div>
   )
