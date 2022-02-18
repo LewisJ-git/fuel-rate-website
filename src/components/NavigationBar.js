e247791c3aa951ddd88405f41854a54b9139b71c
LongNguyen-git <lucasnguyen2112@gmail.com>
2022-02-17 21:11:01 -0600

    Update NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,6 +7,10 @@ const NavigationBar = () => {
             <ul>
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/login'}>Login</Link></li>
+                {/* add authorization when implementing backend */}
+                                {/* |
+                                    |
+                                    v */}
                 <li><Link to={'/history'}>Quote History</Link></li>
                 <li><Link to={'/quote'}>Quote</Link></li>
                 <li><Link to={'/profile'}>Profile</Link></li>
