da8ee70ba29a3772945bd3492b61f822def1631a
nghoang1 <57956036+nghoang1@users.noreply.github.com>
2022-03-10 00:28:33 -0600

    add get form data

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,32 +1,47 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 function Quote(){
-    //user address
-    function handleChange(event) {
-        console.log(event.target.value);
+    const url = ''
+    const [data,setData]= useState({
+    gallon:"",
+    date:""
+    })
+    
+    function handle(e){
+        const newData = {...data}
+        newData[e.target.id] = e.target.value
+        setData(newData)
+        console.log(newData);
     }
-    //get current date
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-    //get suggested Price / gallon
+
+    function submit(e){
+        e.preventDefault();
+        axios.post(url,{
+            gallon: data.gallon,
+            date: data.date
+        })
+        .then(res=>{
+            console.log(res.data);
+        })
+    }
+    
     var suggest_price = "$3.00"
     //total price
     var total_price = "$250.00"
-    today =  yyyy+'-'+ mm +'-' +dd;
+   
     
     return(
     <div className='Fuelquote'>
         <h1>Fuel Quote Form</h1>    
-        <form>
+        <form onSubmit={(e)=> submit(e)}>
             <label className="form-label-quote">Gallon Requested</label>
-            <input type = "number" onChange={handleChange} placeholder="Enter number of gallons" ></input>
+            <input type = "number" onChange={(e)=>handle(e)} id="gallon" value={data.gallon} placeholder="Enter number of gallons" ></input>
             
             <label className="form-label-quote">Delivery Date</label>
-            <input type="date" onChange={handleChange} value={today}></input>
+            <input type="date" onChange={(e)=>handle(e)} id="date" value={data.date}></input>
 
             <label className="form-label-quote">Delivery Address</label>
-            <input type="text" onChange={handleChange} placeholder="Enter the delivery address" ></input>
+            <input type="text"  placeholder="Enter the delivery address" ></input>
 
             <label id="pricelabel1">
                 Suggested Price<h3>{suggest_price}</h3>
@@ -36,6 +51,7 @@ function Quote(){
                 Total Price
                 <h3>{total_price}</h3>
             </label>
+            
         </form>
     </div>
 
