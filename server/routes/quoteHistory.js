e693d68190d52217bab6c7aedef6cc9e4a6402d3
nhoang0925 <nhoang0925@gmail.com>
2022-03-12 23:40:28 -0600

    history routing bug fix

--- server/routes/quoteHistory.js
+++ server/routes/quoteHistory.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/history", (req, res) => {
+router.get("/quote", (req, res) => {
   
   var userID = "Nhi Hoang";
   var gallons_requested = "500";
