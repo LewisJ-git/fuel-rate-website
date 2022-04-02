fc9cf110e644d668b2332326a6515d1e4cb41236
nhoang0925 <nhoang0925@gmail.com>
2022-04-02 07:59:35 -0500

    db query for quoteHistory backend

--- server/routes/quoteHistory.js
+++ server/routes/quoteHistory.js
@@ -1,51 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
-//router.post("/", (req, res) => {
-    //let body = req.body;
-    //let quoted = {
-        //customer_id: body.customer_id,
-        //gallons_requested: body.gallons_requeste,
-        //delivery_address: body.delivery_address,
-        //delivery_date: body.delivery_date,
-        //suggested_price: body.suggested_price,
-        //total_due: body.total_due,
-    //};
-  
-    //console.log(req.body);
-
-    //let response = {};
-        //return res.send(response);
-  //});
+const db = require("../database");
 
 router.get("/quote", (req, res) => {
-    const abc = [
-        {
-            id: 1,
-            gallons_requested: "500",
-            delivery_address: "4800 Calhoun Rd, Houston, TX 77004",
-            delivery_date: "03/01/2022",
-            suggested_price: "3.00",
-            total_due: "1500.00",
-        },
-        {
-            id: 2,
-            gallons_requested: "1000",
-            delivery_address: "4800 Calhoun Rd, Houston, TX 77004",
-            delivery_date: "03/05/2022",
-            suggested_price: "3.00",
-            total_due: "3000.00",
-        },
-        {
-            id: 3,
-            gallons_requested: "3000",
-            delivery_address: "4800 Calhoun Rd, Houston, TX 77004",
-            delivery_date: "03/08/2022",
-            suggested_price: "3.00",
-            total_due: "9000.00",
-        }
-    ];
-    res.json(abc);
-});
-
-module.exports = router;
+  
+    let user_id = parseInt(req.query.user_id);
+    let sql = "SELECT quotes.quote_id, gallon, profiles.address1, address2, city, state, zipcode, quotes.delivery, suggestedPrice, totalPrice FROM users, profiles, quotes WHERE quotes.user_id=? AND users.user_id = profiles.user_id AND profiles.user_id = quotes.user_id;";
+  
+    let response = {};
+    db.query(sql, [user_id], (error, result) => {
+      if (error) throw error;
+      response = JSON.parse(JSON.stringify(result));
+      return res.send(response);
+    });
+  });
+  
+  module.exports = router;
